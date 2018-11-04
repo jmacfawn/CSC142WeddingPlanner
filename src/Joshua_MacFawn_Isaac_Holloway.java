e37bebcf5bace393176a3e23585185e086389488
Joshua MacFawn <joshuamacfawn@gmail.com>
2018-11-03 19:17:54 -0700

    Started file for submission. Added description of project

---
+++ src/Joshua_MacFawn_Isaac_Holloway.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * CSC 142 group project #1: Wedding planner program. 
+ * Business need is to produce a program to assist a wedding 
+ * planner with seating arrangements. Input is number of guests
+ * and max table size. Output is number of max and max-1 tables 
+ * along with guests assigned to each table.    
+ * @author Joshua MacFawn and Isaac Holloway
+ *
+ *
+ */
+public class Joshua_MacFawn_Isaac_Holloway {
+
+	/**
+	 * 
+	 */
+	public Joshua_MacFawn_Isaac_Holloway() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Test {
@@ -8,18 +6,24 @@ public class Test {
 	private static String file = "500names.txt";
 
 	public static void main(String[] args) throws FileNotFoundException{
+		
+		String[] array = {"items", "and", "more"}; 
 
 		try {
 			Scanner sc = new Scanner(new File("500names.txt"));
+						
+			int j = 0;
 			
-			System.out.println("We got the file");
-	
-			// your code to read the file must be added
-		        // here.  Use sc.nextLine() to read each line as a String	
-				 
+			for(int i = 0; i < 10; i++ ) {
+				System.out.println(sc.nextLine());
+				
+			}
+		
+			sc.close();
 		}	
 					
-	
+		
+		
 		catch (IOException e) {
 		 // TODO Auto-generated catch block
 		  e.printStackTrace();
